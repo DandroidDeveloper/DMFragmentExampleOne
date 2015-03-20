32b3dd3150b1c3de6a41405c3226b90e593fe745
Dmarcoux111 <dangmarcoux@gmail.com>
2015-03-19 22:30:26 -0400

    Initial commit

---
+++ app/src/main/java/com/chaos/dmfragmentexampleone/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chaos.dmfragmentexampleone;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_fragment_demo,
+                menu);
+        return true;
+    }
+}
